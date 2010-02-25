6dbedb33f802f4f283e54c52fde83fa8f1791218
alexbarfoot <alexbarfoot@8542e829-9606-4c25-ab2e-0833686b1b04>
2010-02-25 20:54:26 +0000

    -added license headers to files

--- SimpleMathParser/src/net/smplmathparser/BinaryOperator.java
+++ SimpleMathParser/src/net/smplmathparser/BinaryOperator.java
@@ -1,4 +1,22 @@
-package net.smplmathparse;
+/*	Copyright (C) 2010  Alex Barfoot
+ 
+ 	This file is part of SimpleMathParser.
+
+    SimpleMathParser is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SimpleMathParser is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SimpleMathParser.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.smplmathparser;
 
 /**
  * Class used to represent a binary operator
---
+++ SimpleMathParser/src/net/smplmathparser/Constant.java
@@ -0,0 +1,56 @@
+/*	Copyright (C) 2010  Alex Barfoot
+ 
+ 	This file is part of SimpleMathParser.
+
+    SimpleMathParser is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SimpleMathParser is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SimpleMathParser.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.smplmathparser;
+
+/**
+ * Class representing a constant
+ * 
+ * @author Alex Barfoot
+ * 
+ */
+
+public class Constant {
+	/**
+	 * The string that represents the name of this variable
+	 */
+	private final String name;
+	/**
+	 * The current value of this variable
+	 */
+	private final double value;
+
+	public Constant(String name, double value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	/**
+	 * @return the name of this constant
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the value of this constant
+	 */
+	public double getValue() {
+		return value;
+	}
+}
